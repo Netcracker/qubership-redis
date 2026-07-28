100ab125508d84886bad7fa587cfca4432aa9995
nach0522 <gowthami.chinthala@netcracker.com>
2026-07-28 15:34:32 +0530

    fix: add context check

--- redis-operator/controllers/dbaasredisadapter_controller.go
+++ redis-operator/controllers/dbaasredisadapter_controller.go
@@ -19,11 +19,14 @@ package controllers
 import (
 	"context"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -33,6 +36,15 @@ import (
 	netcrackercomv2 "github.com/Netcracker/qubership-redis/redis-operator/api/v2"
 	"github.com/Netcracker/qubership-redis/redis-operator/api/v2/impl"
 )
+
+const specConfigMapName = "last-applied-configuration-info"
+
+func onlySpecConfigMap() predicate.Predicate {
+	nameMatches := func(obj client.Object) bool {
+		return obj.GetName() == specConfigMapName
+	}
+	return predicate.NewPredicateFuncs(nameMatches)
+}
 
 // DbaasRedisAdapterReconciler reconciles a DbaasRedisAdapter object
 type DbaasRedisAdapterReconciler struct {
@@ -74,6 +86,11 @@ func (r *DbaasRedisAdapterReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	r.Reconciler = newReconciler(mgr)
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&netcrackercomv2.DbaasRedisAdapter{}).
+		Watches(
+			&corev1.ConfigMap{},
+			handler.EnqueueRequestForOwner(mgr.GetScheme(), mgr.GetRESTMapper(), &netcrackercomv2.DbaasRedisAdapter{}),
+			builder.WithPredicates(onlySpecConfigMap()),
+		).
 		WithEventFilter(ignoreStatusUpdatePredicate()).
 		Complete(r)
 }
